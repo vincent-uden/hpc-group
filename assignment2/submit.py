4d80517845901ab9c92dd91cd539aabeb95d8f0d
Vincent Udén <vincentuden@gmail.com>
2023-09-29 14:14:19 +0200

    Update submission script

--- assignment2/submit.py
+++ assignment2/submit.py
@@ -1,25 +1,31 @@
 import os
 import shutil
+import argparse
 
 makefile_template = """
 CFLAGS = -O3 -fopenmp -lm -march=native
 
 .PHONY : all
-all : main
+all : TARGET
 
-main : SOURCE_FILES
+TARGET : SOURCE_FILES
 \tgcc -o ./$@ $^ $(CFLAGS)
 
 .PHONY : clean
 clean :
-\trm -f ./main
+\trm -f ./TARGET
 """
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create a submission archive.")
+    parser.add_argument("target", help="Name of the target binary", default="main")
+    args = parser.parse_args()
+
     src_files = os.listdir("./src")
     src_files = list(filter(lambda x: (not x.startswith("benchmark")) and (not x.startswith("test")),src_files))
 
     makefile_str = makefile_template.replace("SOURCE_FILES", " ".join(filter(lambda x: x[-1] != "h", src_files)))
+    makefile_str = makefile_str.replace("TARGET", args.target)
 
     if os.path.exists("submit"):
         shutil.rmtree("submit")
