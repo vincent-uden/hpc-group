5e08ec5c86996722e31e611482f31f2fc04b7524
deric2019 <ericle220@gmail.com>
2023-10-11 11:15:00 +0200

    Cli and slave threads

--- assignment3/src/cli.c
+++ assignment3/src/cli.c
@@ -14,16 +14,15 @@ CliArgs parse_cli(int argc, char** argv) {
     int opt_index = 0;
 
     static struct option long_options[] = {
-        // long_name , ?argument required, default, short_name
+        // long_name , ?argument required, flag, short_name
         {"threads",  required_argument , 0, 't'},
         {"lines", required_argument, 0, 'l'},
         {"verbose", optional_argument, 0, 'v'},
-        {"degree", required_argument, 0, 'd'},
         {"help", no_argument, 0, 'h'},
         {0, 0, 0, 0}
     };
 
-    while ((c = getopt_long(argc, argv, "t:l:d:vh", long_options, &opt_index)) != -1) {
+    while ((c = getopt_long(argc, argv, "t:l:vh", long_options, &opt_index)) != -1) {
         switch (c) {
             case 't':
                 args.threads = atoi(optarg);
@@ -31,33 +30,39 @@ CliArgs parse_cli(int argc, char** argv) {
             case 'l':
                 args.rows = atoi(optarg);
                 break;
-            case 'd':
-                args.degree = atoi(optarg);
-                break;
             case 'v':
                 args.verbose = 1;
                 break;
             case 'h':
-                printf("Usage: cell_distances [OPTIONS]\n");
-                printf("Calculate cell distances using parallization.\n");
+                printf("Usage: newton [OPTIONS]\n");
+                printf("Computes how complex plane points converge to function roots.\n");
                 printf("\n");
                 printf("Options:\n");
                 printf("    -t, --threads <num_threads>   Specify the number of threads (positive integer), Default []\n");
                 printf("    -l, --lines <lines>           Specify the number of rows Default []\n");
-                printf("    -d, --degree <degree>         Specify the degree of the polynomial  Default []\n");
-                printf("    -v, --verbose <versbose>      Use verbose output\n");
+                printf("    -v, --verbose <verbose>      Use verbose output\n");
                 printf("    -h, --help                    Display this help message\n");
                 printf("\n");
                 printf("Description:\n");
-                printf("    This program calculates cell distances using multi-threading to speed up the process.\n");
-                printf("    You can specify the number of threads and the chunk size to control the computation.\n");
+                printf("    This program computes how complex plane points converge to function roots\n");
+                printf("    and plot two images: one colors each pixel based on the root of convergence,");
+                printf("    while the other represents the number of iterations needed to approximate a root \n");
+                printf("    The computation occurs in parallel using the C11 threads\n");
+                printf("    You can specify the number of threads, rows and columns, and the degree of the polynomial f(x) = x^d-1 \n");
                 printf("\n");
                 printf("Example usage:\n");
-                printf("    cell_distances -t 4 -c 1000\n");
+                printf("    newton -t5 -l1000 3\n");
                 exit(0);
                 break;
         }
     }
+
+    if (optind >= argc) {
+        fprintf(stderr, "Degree of the polynomial is missing.\n");
+        exit(EXIT_FAILURE);
+    }
+    args.degree = atoi(argv[optind]);
+    
     return args;
 }
 
--- assignment3/src/slave.c
+++ assignment3/src/slave.c
@@ -1,5 +1,47 @@
 #include "slave.h"
 
-void slave_main(void *args){
-    
+Result **result;
+char *write_queue;
+
+// Slave Threads
+int row_index;
+int row_size;
+mtx_t row_index_mtx;
+
+// Master Thread
+mtx_t work_mtx;
+cnd_t work_done;
+size_t next_row_to_write = 0;
+
+void slave_main(void *_args){
+    // Start working on rows
+    while (row_index < row_size) {
+        // Lock mutex for row index
+        mtx_lock(&row_index_mtx);
+
+         // Map im part [-2, 2], starting from 2 -> -2
+        float im = 2.0 - row_index*4.0/row_size;
+        float re; 
+
+        // Loop to generate complex numbers for row index
+		for (size_t j = 0; j < row_size; j++){
+
+            // Map re part into [-2, 2], starting from -2 -> 2
+            re = j*4.0/row_size - 2.0;
+
+            // Define the complex number
+			float complex z = re + im*I;
+
+            // Compute newton 
+			newton(z, result[row_index]);
+		}
+
+        // Increment row index
+        if (row_index < row_size) {
+            write_queue[row_index++] = 1;
+            cnd_signal(&work_done);
+        }
+        // Unlock mutex 
+        mtx_unlock(&row_index_mtx);
+    }
 }
