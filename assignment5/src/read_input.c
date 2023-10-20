2ed0023d5884883b4b00259a44bad0909eabf856
deric2019 <ericle220@gmail.com>
2023-10-20 14:30:58 +0200

    Fixed input

--- assignment5/src/read_input.c
+++ assignment5/src/read_input.c
@@ -14,15 +14,20 @@ float * read_data(int mpi_rank, int nmb_mpi_proc, size_t *rows, size_t *cols) {
         printf("ERROR: could not find file init ...\n");
         exit(1);
     }
-    
-    // Read the first row of the which is the size: [rows, cols]
-    fscanf(file, "%zu %zu\n", rows, cols);
-    // printf("box [%zu x %zu]\n", *rows, *cols);
 
+    size_t global_rows;
+    // Read the first row of the which is the size: [rows, cols]
+
+    fscanf(file, "%zu %zu\n", &global_rows, cols);
+    // printf("box [%zu x %zu]\n", *rows, *cols);
+    
     // Calculate the number of rows each worker will process
-    int rows_per_worker = *rows / nmb_mpi_proc;
-    int remainder_rows = *rows % nmb_mpi_proc;
+    int rows_per_worker = global_rows / nmb_mpi_proc;
+    int remainder_rows = global_rows % nmb_mpi_proc;
+    
     int my_rows = (mpi_rank < remainder_rows) ? (rows_per_worker + 1) : rows_per_worker;
+    *rows = my_rows;
+    
     int row_zero = rows_per_worker * mpi_rank + min(mpi_rank, remainder_rows);
 
     
@@ -30,6 +35,7 @@ float * read_data(int mpi_rank, int nmb_mpi_proc, size_t *rows, size_t *cols) {
     float *data = (float *)calloc((my_rows+2) * (*cols + 2), sizeof(float));
 
     if (data == NULL) {
+
         printf("Memory allocation failed.\n");
         fclose(file);
         exit(1);
@@ -43,7 +49,7 @@ float * read_data(int mpi_rank, int nmb_mpi_proc, size_t *rows, size_t *cols) {
     while(fscanf(file, "%d %d %f", &x, &y, &val) != EOF) {
         if (x >= row_zero && x < (row_zero + my_rows)){
             // printf(" x y v = %d %d %f\n", x, y, val);
-            data[(x-row_zero + 1)*(*cols+2) + (y+1)] = val;
+            data[(x - row_zero + 1)*(*cols+2) + (y+1)] = val;
         }
     }
     
@@ -57,21 +63,22 @@ int main(int argc, char** argv) {
 
     float *data;
     size_t rows, cols;
+    int nmb_mpi_proc = 3;
 
-    data = read_data(&rows, &cols);
+    for (int mpi_rank = 0; mpi_rank < nmb_mpi_proc; ++mpi_rank ){
+        data = read_data(mpi_rank, nmb_mpi_proc, &rows, &cols);
 
-    for (size_t i = 0; i < (rows + 2) * (cols + 2); i++) {
-        if (i % (cols + 2) == 0) {
-            printf("\n");
+        for (size_t i = 0; i < (rows + 2) * (cols + 2); i++) {
+            if (i % (cols + 2) == 0) {
+                printf("\n");
+            }
+            printf("%.6f   ", data[i]);
         }
-        printf("%.6f   ", data[i]);
-    }
-
-    printf("\nRows: %zu\n", rows);
-    printf("Cols: %zu \n", cols);
-    printf("Data_element: %.6f", data[12]);
-
+        printf("\n");
+        printf("Rows: %d", rows);
     free(data);
+
+    }
     return 0;
 }
 #endif
