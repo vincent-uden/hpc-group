f855cda66e59a963d7954c8f2cd96a3de3c21697
Vincent Udén <vincentuden@gmail.com>
2023-10-20 18:34:33 +0200

    ?

--- assignment5/src/main.c
+++ assignment5/src/main.c
@@ -56,7 +56,7 @@ main(int argc, char **argv)
         float *data_last = padding_down - cols - 2;
 
         MPI_Status status;
-        if (nmb_mpi_proc > 1) {
+        if (nmb_mpi_proc > 1 && mpi_rank % 2 == 0) {
             if ( mpi_rank == 0) {
                 MPI_Sendrecv(data_last, cols, MPI_FLOAT, 1, step,
                             padding_down, cols, MPI_FLOAT, 1, step,
