7983b2db518f51aaf496158c507bcc9069e904cf
deric2019 <ericle220@gmail.com>
2023-10-20 13:17:59 +0200

    Implemented input

--- assignment5/src/read_input.c
+++ assignment5/src/read_input.c
@@ -1,21 +1,28 @@
 #include "read_input.h"
 
-float * read_data(size_t *rows, size_t *cols) {
+float * read_data(int mpi_rank, int nmb_mpi_proc, size_t *rows, size_t *cols) {
     // Create file pointer
     FILE *file = fopen("init", "r");
 
     // Error handling
-	if(file==NULL) {
-	 	printf("ERROR: could not find file init ...\n");
+    if(file==NULL) {
+        printf("ERROR: could not find file init ...\n");
         exit(1);
-	}
-
+    }
+    
     // Read the first row of the which is the size: [rows, cols]
-	fscanf(file, "%zu %zu\n", rows, cols);
+    fscanf(file, "%zu %zu\n", rows, cols);
     // printf("box [%zu x %zu]\n", *rows, *cols);
 
+    // Calculate the number of rows each worker will process
+    int rows_per_worker = *rows / nmb_mpi_proc;
+    int remainder_rows = *rows % nmb_mpi_proc;
+    int my_rows = (mpi_rank < remainder_rows) ? (rows_per_worker + 1) : rows_per_worker;
+    int row_zero = rows_per_worker * mpi_rank + min(mpi_rank, remainder_rows);
+
+    
     // Initialize data matrix
-    float *data = (float *)calloc((*rows + 2) * (*cols + 2), sizeof(float));
+    float *data = (float *)calloc((my_rows) * (*cols + 2), sizeof(float));
 
     if (data == NULL) {
         printf("Memory allocation failed.\n");
@@ -25,15 +32,17 @@ float * read_data(size_t *rows, size_t *cols) {
 
     // Initialize coordinates and val
     int x, y;
-	float val;
+    float val;
 
     // Read values for each coordinates
-	while(fscanf(file, "%d %d %f", &x, &y, &val) != EOF) {
-		// printf(" x y v = %d %d %f\n", x, y, val);
-		data[(x+1)*(*cols+2) + (y+1)] = val;
-	}
-
-	fclose(file);
+    while(fscanf(file, "%d %d %f", &x, &y, &val) != EOF) {
+        if (x >= row_zero && x < (row_zero + my_rows)){
+            // printf(" x y v = %d %d %f\n", x, y, val);
+            data[(x)*(*cols+2) + (y+1)] = val;
+        }
+    }
+    
+    fclose(file);
 
     return data;
 }
