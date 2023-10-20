cfd7389081c2a13df86851312e5d0902da42cebe
Vincent Udén <vincentuden@gmail.com>
2023-10-20 17:59:51 +0200

    Puckad??

--- assignment5/src/diffusion.c
+++ assignment5/src/diffusion.c
@@ -1,8 +1,8 @@
 #include "diffusion.h"
 
 void diffusion_step(double* prev_step, double* next_step, int rows, int cols, double c) {
-    for (size_t i = 1; i < rows; ++i) {
-        for (size_t j = 1; j < cols; ++j) {
+    for (size_t i = 1; i <= rows; ++i) {
+        for (size_t j = 1; j <= cols; ++j) {
             int index = i * (cols + 2) + j;
             next_step[index] = prev_step[index] + c * ((
                 prev_step[index - 1] +
