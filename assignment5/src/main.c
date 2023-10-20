7ef8020223be49978ff66ab8c620423d07e2bb21
Simon Paulsson <simon@paulsson.one>
2023-10-20 13:36:17 +0200

    varbose mode

--- assignment5/src/main.c
+++ assignment5/src/main.c
@@ -28,10 +28,15 @@ main(int argc, char **argv)
     float *dataA = read_data(mpi_rank, nmb_mpi_proc, &rows, &cols);
     float *dataB = calloc((rows + 2)*(cols+2), sizeof(float));
 
+    if (args.verbose && mpi_rank == 0)
+        printf("Data read\n");
 
     for ( size_t step = 0; step < args.n_iter; step++) {
         float *data = step % 2 ? dataB : dataA;
         float *next_data = step % 2 ? dataA : dataB;
+
+        if (args.verbose && mpi_rank == 0)
+            printf("In step %lu\n", step);
 
         // Sync BC
         float *padding_up = data + 1;
@@ -65,6 +70,9 @@ main(int argc, char **argv)
         diffusion_step(data, next_data, rows, cols, args.diff_c);
     }
 
+    if (args.verbose && mpi_rank == 0)
+            printf("Loop done\n");
+
     float *data = args.n_iter % 2 ? dataB : dataA;
 
     // Calculate average
@@ -81,6 +89,9 @@ main(int argc, char **argv)
     float avg;
     MPI_Allreduce(&internal_avg, &avg, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
 
+    if (args.verbose && mpi_rank == 0)
+        printf("Average calculated to %f\n", avg);
+
     // Calculate abs diff
     float abs_diff;
     for ( size_t i = 1; i <= rows; i ++) {
@@ -94,6 +105,9 @@ main(int argc, char **argv)
     // Reduce to mpi rank 0
     float reduce_abs_diff;
     MPI_Reduce(&abs_diff, &reduce_abs_diff, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (args.verbose && mpi_rank == 0)
+        printf("Abs diff Average calculated to %f\n", reduce_abs_diff);
 
     MPI_Finalize();
 
