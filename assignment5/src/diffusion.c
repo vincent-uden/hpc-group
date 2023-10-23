dac12031c441cd37d1b6114af8c1fbd47e5a1095
Simon Paulsson <simon@paulsson.one>
2023-10-23 11:34:08 +0200

    fix padding

--- assignment5/src/diffusion.c
+++ assignment5/src/diffusion.c
@@ -5,8 +5,8 @@ void diffusion_step(float* prev_step, float* next_step, int rows, int cols, floa
     // row major
     for (size_t i = 1; i <= rows; ++i) {
         float *read_row = prev_step + i * (cols + 2);
-        float *read_row_above = prev_step + (i - 1) * (cols + 2) + 1;
-        float *read_row_below = prev_step + (i + 1) * (cols + 2) + 1;
+        float *read_row_above = prev_step + (i - 1) * (cols + 2);
+        float *read_row_below = prev_step + (i + 1) * (cols + 2);
         float *write_row = next_step + i * (cols + 2);
 
         __builtin_prefetch(read_row, 0, 1);
